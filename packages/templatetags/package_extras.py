ee137e988d0fff8058e2a9b24277420755a6fde9
osa1 <omeragacan@gmail.com>
2011-08-24 10:37:48 +0300

    remove unnecessary tags

--- packages/templatetags/package_extras.py
+++ packages/templatetags/package_extras.py
@@ -1,11 +1,10 @@
-from urllib import urlencode, quote as urlquote
+from urllib import urlencode
 try:
     from urlparse import parse_qs
 except ImportError:
     from cgi import parse_qs
 
 from django import template
-from django.utils.html import escape
 from django.template.defaultfilters import stringfilter
 
 register = template.Library()
@@ -37,54 +36,6 @@ def do_buildsortqs(parser, token):
                 "%r tag's argument should be in quotes" % tagname)
     return BuildQueryStringNode(sortfield[1:-1])
 
-@register.simple_tag
-def userpkgs(user):
-    if user:
-        # TODO don't hardcode
-        title = escape('View packages maintained by ' + user.username)
-        return '<a href="/packages/?maintainer=%s" title="%s">%s</a>' % (
-                user.username,
-                title,
-                user.username,
-        )
-    return ''
-
-
-def svn_link(package, svnpath):
-    '''Helper function for the two real SVN link methods.'''
-    parts = (package.repo.svn_root, package.pkgbase, svnpath)
-    parts = ("", "", "")  # TODO
-    linkbase = "http://projects.archlinux.org/svntogit/%s.git/tree/%s/%s/"
-    return linkbase % tuple(urlquote(part) for part in parts)
-
-@register.simple_tag
-def svn_arch(package):
-    distribution = package.repo.name.lower()
-    return svn_link(package, "repos/%s-%s" % (distribution, package.architecture.name))
-
-@register.simple_tag
-def svn_trunk(package):
-    return svn_link(package.name, "trunk")
-
-@register.simple_tag
-def bugs_list(package):
-    data = {
-        #'project': package.repo.bugs_project,
-        'project': "",
-        'string': package.name,
-    }
-    return "https://bugs.archlinux.org/?%s" % urlencode(data)
-
-@register.simple_tag
-def bug_report(package):
-    data = {
-        #'project': package.repo.bugs_project,
-        'project': "",
-        #'product_category': package.repo.bugs_category,
-        'product_category': "",
-        'item_summary': '[%s]' % package.name,
-    }
-    return "https://bugs.archlinux.org/newtask?%s" % urlencode(data)
 
 @stringfilter
 def replace(name, char):
